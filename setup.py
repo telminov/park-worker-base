a94f7f3cead6871654ea5eed33e7ef706166f658
sergey@telminov.ru <sergey@telminov.ru>
2015-11-05 19:17:48 +0300

    distribution by docker

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from distutils.core import setup
 
 setup(
     name='park-worker-base',
-    version='0.0.1',
+    version='0.0.3',
     description='Base logic of workers for park-keeper project.',
     author='Telminov Sergey',
     url='https://github.com/telminov/park-worker-base',
-    packages=['parkworker',],
+    packages=[
+        'parkworker',
+        'parkworker/monits',
+    ],
     license='The MIT License',
     install_requires=[
         'pytz',
