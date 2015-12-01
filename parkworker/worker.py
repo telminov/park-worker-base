cd5dd7f742c5c3d7096d655cb321e54c442bd1c6
sergey@telminov.ru <sergey@telminov.ru>
2015-12-01 15:54:16 +0300

    simple remote command work

--- parkworker/worker.py
+++ parkworker/worker.py
@@ -141,7 +141,8 @@ class BaseWorker(multiprocessing.Process):
         self._emit_task(task_data, task_type)
 
     def _register_complete_task(self, task_data, task_type):
-        self.emit_event(MONIT_STATUS_EVENT, json.dumps(task_data, default=json_util.default))
+        if task_type == TASK_TYPE_MONIT:
+            self.emit_event(MONIT_STATUS_EVENT, json.dumps(task_data, default=json_util.default))
         self._emit_task(task_data, task_type)
         self._emit_worker({'complete_task': task_data})
 
